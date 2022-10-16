8d01368447223fc61479381038dd02615a2c7c32
Gabriel Lucizano <69920136+lucizzz@users.noreply.github.com>
2022-10-16 00:14:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo='lista')
+    return render_template('lista.html', titulo='aaa')
 
 @app.route('/criar', methods=['POST',])
 def criar():
     numero = request.form['numero']
-    numero2 = request.form['numero2']
-    if numero.isnumeric() and numero2.isnumeric(): #isnumeric() função do python para verificar se é um numero
-        numero = int(numero)
-        numero2 = int(numero2)
-        resul = numero+numero2
-        return render_template('lista.html', titulo='lista', aa=resul)
-    else:
-        return render_template('lista.html', titulo='lista', aa="Não é int")
-
-
-
+    return render_template('lista.html', novo = numero)
 
 
 
