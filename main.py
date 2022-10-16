1e2d8ffb8fd1192a0db4ee2c85b7c17de16f550c
Gabriel Lucizano <69920136+lucizzz@users.noreply.github.com>
2022-10-16 12:30:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+# test
 from flask import Flask
 from flask import Flask, render_template, request
+
+def frequencia(texto, palavra):
+    lista_texto = texto.split()
+    # lista_palavra = palavra.split()
+    cont = 0
+
+    for i in lista_texto:
+        if palavra == i:
+            cont+=1
+    
+    return cont
+
 
 app = Flask(__name__)
 
@@ -7,10 +20,12 @@ app = Flask(__name__)
 def index():
     return render_template('lista.html', titulo='aaa')
 
-@app.route('/criar', methods=['POST',])
-def criar():
-    numero = request.form['numero']
-    return render_template('lista.html', novo = numero)
+@app.route('/calcular', methods=['POST',])
+def calcular():
+    texto = request.form['texto']
+    palavra = request.form['palavra']
+    res = frequencia(texto, palavra)
+    return render_template('lista.html', novo = res)
 
 
 
